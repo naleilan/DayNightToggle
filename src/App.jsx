756fc3bc8cffa1b3b48d015f3c58cc444e531c57
naleilan <leilan.n@gmail.com>
2024-01-23 21:31:40 +0300

    done

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [night, setNight] = useState(false);
+
+  return (
+    <div className="base">
+      <Switch onClick={() => setNight(!night)} night={night} />
+    </div>
+  );
+}
+
+const Switch = ({ onClick, night }) => {
+  return (
+    <div
+      className={`switch-wrapper ${!night ? "day" : "night"}`}
+      tabIndex={-1}
+      role="button"
+      onClick={onClick}
+    >
+      <div className={`sun-moon-combo ${!night ? "sun" : "moon"}`}>
+        <div className="moon-elem"></div>
+        <div className="moon-elem"></div>
+        <div className="moon-elem"></div>
+      </div>
+      <div className={`raybase ${night ? "hide" : "show"}`}>
+        <div className="ray-1"></div>
+        <div className="ray-2"></div>
+        <div className="ray-3"></div>
+      </div>
+      <div className={`raybase-2 ${!night ? "hide" : "show"}`}>
+        <div className="ray-1"></div>
+        <div className="ray-2"></div>
+        <div className="ray-3"></div>
+      </div>
+      {night && (
+        <div className="stars">
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          {[...Array(15)].map((_, index) => (
+            <div key={index} className="round-star"></div>
+          ))}
+        </div>
+      )}
+
+      <div className="clouds-line-1" />
+      <div className="clouds-line-2" />
+    </div>
+  );
+};
